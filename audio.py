78a8ab31b51fd18ab4458099833b3b8f24bdd7da
tim <timothyportnoff@gmail.com>
2023-03-17 18:45:18 -0700

    Moved 'search' and 'target acquired' audio from main.py to audio.py

--- audio.py
+++ audio.py
@@ -1,10 +1,12 @@
 import pygame
+import random
 from time import sleep
 
-#file setup
+#global variables
 file_path = "sounds/"
 file_ext = ".wav"
-speaker_volume = 0.3 
+speaker_volume = 0.1 
+prev_sound = "null"
 
 def play_audio(file_name):
     pygame.mixer.init()
@@ -14,12 +16,10 @@ def play_audio(file_name):
     return
 
 def play_audio_loop(file_name, loop):
-    #for i in range(loop):
     pygame.mixer.init()
     pygame.mixer.music.set_volume(speaker_volume)
     pygame.mixer.music.load(file_path + file_name + file_ext)
     pygame.mixer.music.play(loop)
-    #sleep(.3)
     return
 
 def play_seinfeld():
@@ -27,7 +27,60 @@ def play_seinfeld():
     pygame.mixer.music.set_volume(1)
     pygame.mixer.music.load("sounds/SeinfeldTheme.mp3")
     pygame.mixer.music.play()
-    #while pygame.mixer.music.get_busy() == True:
-    #    continue
     return
 
+def play_finding():
+    #Pick random path and play sound
+    sound_path = random.randrange(0, 7)
+    if sound_path == prev_sound: sound_path = random.randrange(0, 7)
+    if#print(sound_path)
+    if sound_path == 0:
+        play_audio("is_anyone_there")
+    elif sound_path == 1:
+        play_audio("hi_question")
+    elif sound_path == 2:
+        play_audio("who's_there")
+    elif sound_path == 3:
+        play_audio("come_over_here")
+    elif sound_path == 4:
+        play_audio("hello_question")
+    elif sound_path == 5:
+        play_audio("canvasing")
+    elif sound_path == 6:
+        play_audio("searching")
+    return
+
+def play_found():
+    #Pick random path and play sound
+    sound_path = random.randrange(0, 9)
+
+    #print(sound_path)
+    if sound_path == 0:
+        play_audio("acquired")
+    elif sound_path == 1:
+        play_audio("i_see_you")
+    elif sound_path == 2:
+        play_audio("ahahaha")
+    elif sound_path == 3:
+        play_audio("firing")
+    elif sound_path == 4:
+        play_audio("gotcha")
+    elif sound_path == 5:
+        play_audio("hello")
+    elif sound_path == 6:
+        play_audio("hellooo")
+    elif sound_path == 7:
+        play_audio("there_you_are")
+    elif sound_path == 8:
+        play_audio("hold_still")
+
+    #CHECK TO SEE IF BUSY TODO
+    while pygame.mixer.music.get_busy() == True:
+        continue
+    #sleep(2)
+
+    #Play firing sound
+    #play_audio_loop("fire", 10)
+    play_audio("turret_fire")
+    sleep(3)
+    return
--- main.py
+++ main.py
@@ -2,12 +2,13 @@
 import RPi.GPIO as GPIO
 import threading
 import os
+PYGAME_HIDE_SUPPORT_PROMPT = 1
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
-import random
+from time import sleep
 
 #Sentry libs
 from config import *
-from time import sleep
 from led import * 
 from sonar import *
 from audio import *
@@ -18,71 +19,23 @@ try:
         while True:
             #Welcome, and setup
             print("Sentry Mode: Activated.")
-
-            #Start audio
             play_audio("sentry_mode_activated")
-            prev_sound = "null"
 
             #Main Loop:
             while True:
                 led_off(RED)
                 for i in range(20):
-                    print('Measure: ')
                     distance = measure_better_average(GPIO_TRIGGER_1, GPIO_ECHO_1) #check distance
-                    if DEBUG: print('Sonar 1 Distance: ', distance, '.')
+                    #if DEBUG: print("Sonar 1 Distance: ", distance, ".")
+                    if DEBUG: print("Sonar 1 Distance: ", distance, ".")
 
                     if distance < 50:
                         #Update warning LED  
                         led_on(RED)
-
-                        #Pick random path and play sound
-                        sound_path = random.randrange(0, 9)
-
-                        #print(sound_path)
-                        if sound_path == 0:
-                            play_audio("acquired")
-                        elif sound_path == 1:
-                            play_audio("i_see_you")
-                        elif sound_path == 2:
-                            play_audio("ahahaha")
-                        elif sound_path == 3:
-                            play_audio("firing")
-                        elif sound_path == 4:
-                            play_audio("gotcha")
-                        elif sound_path == 5:
-                            play_audio("hello")
-                        elif sound_path == 6:
-                            play_audio("hellooo")
-                        elif sound_path == 7:
-                            play_audio("there_you_are")
-                        elif sound_path == 8:
-                            play_audio("hold_still")
-                        sleep(2)
-
-                        #Play firing sound
-                        #play_audio_loop("fire", 10)
-                        play_audio("turret_fire")
-                        sleep(3)
+                        play_found()
                     #Sleep at the end of the loop. This is a magic number, please adjust.
                     sleep(0.4)
-                #Pick random path and play sound
-                sound_path = random.randrange(0, 7)
-                if sound_path == prev_sound: sound_path = random.randrange(0, 7)
-                #print(sound_path)
-                if sound_path == 0:
-                    play_audio("is_anyone_there")
-                elif sound_path == 1:
-                    play_audio("hi_question")
-                elif sound_path == 2:
-                    play_audio("who's_there")
-                elif sound_path == 3:
-                    play_audio("come_over_here")
-                elif sound_path == 4:
-                    play_audio("hello_question")
-                elif sound_path == 5:
-                    play_audio("canvasing")
-                elif sound_path == 6:
-                    play_audio("searching")
+                play_finding()
 
 #Exit cleanly after a keyboard interrupt
 except KeyboardInterrupt:
